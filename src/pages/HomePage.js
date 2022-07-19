ff190d3d8c9ddd0da9aec74714e1fb9269819cb7
Adam Cahan <adam.cahan@gmail.com>
2022-07-19 18:47:45 -0500

    Readability cleanup

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,9 +2,14 @@ import ArticleList from '../components/ArticleList/ArticleList.js'
 import News from '../data/news.json';
 
 function HomePage() {
+  const handleTitleClick = (articleID) => console.log("TODO - use React Router\'s history.push() method to change the page to /article/${articleID}");
+
   return (
     <div>
-      <ArticleList articles={News} handleTitleClick={(articleID) => console.log("TODO - use React Router\'s history.push() method to change the page to /article/${articleID}") } />
+      <ArticleList 
+        articles={News} 
+        handleTitleClick={handleTitleClick} 
+      />
     </div>
   );
 }
